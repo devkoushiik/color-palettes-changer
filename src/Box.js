82e592e29310dea076e77bf1558bd8afb4950d3e
Koushik Ahmed <koushikxahmed@gmail.com>
2023-01-19 16:35:01 +0600

    all file added

---
+++ src/Box.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./Box.css";
+import { choice } from "./helpers";
+
+class Box extends Component {
+  static defaultProps = {
+    allColors: ["aqua", "teal", "navy", "yellow", "olive", "lime","green"],
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: choice(this.props.allColors),
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  pickColor() {
+    let newColor;
+
+    do{
+        newColor = choice(this.props.allColors);
+    }while(newColor === this.state.color)
+
+    this.setState({color : newColor})
+  }
+
+  handleClick() {
+    this.pickColor();
+  }
+  render() {
+    return (
+      <div>
+        <div
+          className="Box"
+          style={{ backgroundColor: this.state.color }}
+          onClick={this.handleClick}
+        ></div>
+      </div>
+    );
+  }
+}
+
+export default Box;
---
+++ src/BoxContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Box from "./Box";
+import "./BoxContainer.css";
+
+export default class BoxContainer extends Component {
+  static defaultProps = {
+    numBoxes: 18,
+  };
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box />
+    ));
+
+    return (
+      <div>
+        <h4>Click over boxes to change color 😒</h4>
+        <div className="BoxContainer">{boxes}</div>;
+      </div>
+    );
+  }
+}
